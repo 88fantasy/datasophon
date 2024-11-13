d09334f2d9c3dd334a771ebe6e8f8de532c8737b
刘树敏 <liusm189@chinaunicom.cn>
2024-11-13 09:42:40 +0800

    fix: cli init

--- datasophon-cli/src/main/resources/sbin/initAll.sh
+++ datasophon-cli/src/main/resources/sbin/initAll.sh
@@ -9,10 +9,18 @@ BASE_PATH=$(
 echo "Bash Path: ${BASE_PATH}"
 INIT_PATH=$(dirname "${BASE_PATH}")
 INIT_LOG_PATH=${INIT_PATH}/logs
+INIT_BIN_PATH=${INIT_PATH}/bin
 
-echo "datasophon-init:${INIT_PATH}"
 if [ ! -d "INIT_LOG_PATH" ]; then
   mkdir -p $INIT_LOG_PATH
 fi
 
-nohup java -jar datasophon-cli-cli.jar create cluster -p /data/datasophon -a initALL > ${INIT_LOG_PATH}/cli.log 2>&1 &
+echo "ini tar"
+bash ${INIT_BIN_PATH}/init-ssh.sh > ${INIT_LOG_PATH}/init-ssh.log
+
+echo "ini jdk"
+bash ${INIT_BIN_PATH}/init-jdk.sh > ${INIT_LOG_PATH}/init-jdk.log
+
+source /etc/profile
+echo "ini create cluster"
+java -jar datasophon-cli-cli.jar create cluster -p /data/datasophon -a initALL > ${INIT_LOG_PATH}/cli.log
--- datasophon-cli/src/main/resources/sbin/initSingleNode.sh
+++ datasophon-cli/src/main/resources/sbin/initSingleNode.sh
@@ -11,9 +11,9 @@ echo "Bash Path: ${BASE_PATH}"
 INIT_PATH=$(dirname "${BASE_PATH}")
 INIT_LOG_PATH=${INIT_PATH}/logs
 
-echo "datasophon-init:${INIT_PATH}"
 if [ ! -d "INIT_LOG_PATH" ]; then
   mkdir -p $INIT_LOG_PATH
 fi
 
-nohup java -jar datasophon-cli-cli.jar create cluster -p /data/datasophon -a initSingleNode > ${INIT_LOG_PATH}/cliSingle.log  2>&1 &
+source /etc/profile
+java -jar datasophon-cli-cli.jar create cluster -p /data/datasophon -a initSingleNode > ${INIT_LOG_PATH}/cliSingle.log
