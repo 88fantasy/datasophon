ed1100efb484a0604b6fb3ac340736c0cd38c1c9
刘树敏 <liusm189@chinaunicom.cn>
2024-11-15 17:56:54 +0800

    feat: add flinkHistory

--- datasophon-worker/src/main/java/com/datasophon/worker/strategy/FlinkHandlerStrategy.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/FlinkHandlerStrategy.java
@@ -1,5 +1,6 @@
 package com.datasophon.worker.strategy;
 
+import cn.hutool.core.io.FileUtil;
 import com.datasophon.common.Constants;
 import com.datasophon.common.cache.CacheUtils;
 import com.datasophon.common.command.ServiceRoleOperateCommand;
@@ -7,11 +8,6 @@ import com.datasophon.common.utils.ExecResult;
 import com.datasophon.worker.utils.KerberosUtils;
 
 import java.sql.SQLException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import cn.hutool.core.io.FileUtil;
 
 /**
  * flink
@@ -21,15 +17,12 @@ import cn.hutool.core.io.FileUtil;
  */
 public class FlinkHandlerStrategy extends AbstractHandlerStrategy implements ServiceRoleStrategy {
     
-    private static final Logger logger = LoggerFactory.getLogger(FlinkHandlerStrategy.class);
-    
     public FlinkHandlerStrategy(String serviceName, String serviceRoleName) {
         super(serviceName, serviceRoleName);
     }
     
     @Override
     public ExecResult handler(ServiceRoleOperateCommand command) throws SQLException, ClassNotFoundException {
-        
         if (command.getEnableKerberos()) {
             logger.info("start to get flink keytab file");
             String hostname = CacheUtils.getString(Constants.HOSTNAME);
@@ -37,7 +30,7 @@ public class FlinkHandlerStrategy extends AbstractHandlerStrategy implements Ser
             if (!FileUtil.exist("/etc/security/keytab/flink.keytab")) {
                 KerberosUtils.downloadKeytabFromMaster("flink/" + hostname, "flink.keytab");
             }
-            
+
         }
         ExecResult startResult = new ExecResult();
         startResult.setExecResult(true);
