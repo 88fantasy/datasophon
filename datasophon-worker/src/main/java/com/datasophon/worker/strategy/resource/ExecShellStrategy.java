8440292ac4875e8eb1ff46248491c721a48d4777
ruanzhiming <ruanzm@chinaunicom.cn>
2024-12-11 17:45:23 +0800

    update:修改InitStrategy成ExecShellStrategy

--- datasophon-worker/src/main/java/com/datasophon/worker/strategy/resource/ExecShellStrategy.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/resource/ExecShellStrategy.java
@@ -1,19 +1,12 @@
 package com.datasophon.worker.strategy.resource;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.StrUtil;
 import com.datasophon.common.Constants;
-import com.datasophon.common.command.ServiceRoleOperateCommand;
 import com.datasophon.common.utils.ExecResult;
 import com.datasophon.common.utils.PlaceholderUtils;
 import com.datasophon.common.utils.ShellUtils;
-import com.datasophon.worker.utils.JuicefsUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
 import java.util.*;
 
 /**
@@ -21,20 +14,19 @@ import java.util.*;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class InitStrategy extends ResourceStrategy {
+public class ExecShellStrategy extends ResourceStrategy {
 
-    public static final String INIT_TYPE = "init";
+    public static final String EXEC_SHELL_TYPE = "execShell";
 
     private List<String> commands;
 
     @Override
     public String type() {
-        return INIT_TYPE;
+        return EXEC_SHELL_TYPE;
     }
 
     @Override
     public ExecResult exec() {
-//        String template = "juicefs format --storage s3 --bucket http://${minioHost}:${minioApiPort} --access-key ${minioAccessKey} --secret-key ${minioSecretKey} ${} ${juicefsMeta}";
         for (String command : commands) {
             command = PlaceholderUtils.replacePlaceholders(command, this.variables, Constants.REGEX_VARIABLE);
 
