8a1025fbdfbe90340937acee4e504c231862b0cf
ruanzhiming <ruanzm@chinaunicom.cn>
2025-01-02 15:28:56 +0800

    update:增加监控dashboard的DML

--- datasophon-api/src/main/resources/db/migration/1.2.2/V1.2.2__DML.sql
+++ datasophon-api/src/main/resources/db/migration/1.2.2/V1.2.2__DML.sql
@@ -4,3 +4,7 @@ INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_
 INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (83, 'ETCD', 'http://${grafanaHost}:3000/d/etcd_cluster/etcd-cluster-overview?orgId=1&refresh=1m&kiosk');
 INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (84, 'REDIS', 'http://${grafanaHost}:3000/d/e008bc3f-81a2-40f9-baf2-a33fd8dec7ec/redis-dashboard-for-prometheus-redis-exporter-1-x?orgId=1&refresh=1m&kiosk');
 INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (85, 'LOKI', 'http://${grafanaHost}:3000/d/baf7c5ea-5012-4b87-83fc-8fe61fef7c33/loki-metrics-dashboard?orgId=1&refresh=1m&kiosk');
+INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (86, 'NGINX', 'http://${grafanaHost}:3000/d/MsjffzSZz/nginx-exporter?orgId=1&refresh=1m&kiosk');
+INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (87, 'JUICEFS', 'http://${grafanaHost}:3000/d/-hm07csGk/juicefs-dashboard?orgId=1');
+INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (88, 'RESEARCH', 'http://${grafanaHost}:3000/d/9IFafKA4z/jmx-dashboard-basic?orgId=1&var-job=researchplatform');
+INSERT INTO `t_ddh_cluster_service_dashboard` (`id`, `service_name`, `dashboard_url`) VALUES (89, 'OPERATION', 'http://${grafanaHost}:3000/d/9IFafKA4z/jmx-dashboard-basic?orgId=1&var-job=operationbrainserver');
