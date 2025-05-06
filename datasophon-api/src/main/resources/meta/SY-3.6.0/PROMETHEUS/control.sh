56464909f41e07accb296af5d4fbdc34c674420e
刘树敏 <liusm189@chinaunicom.cn>
2025-05-06 11:52:36 +0800

    fix: prometheus数据存储30天

--- datasophon-api/src/main/resources/meta/SY-3.6.0/PROMETHEUS/control.sh
+++ datasophon-api/src/main/resources/meta/SY-3.6.0/PROMETHEUS/control.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#
-#  Licensed to the Apache Software Foundation (ASF) under one or more
-#  contributor license agreements.  See the NOTICE file distributed with
-#  this work for additional information regarding copyright ownership.
-#  The ASF licenses this file to You under the Apache License, Version 2.0
-#  (the "License"); you may not use this file except in compliance with
-#  the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
-
 usage="Usage: start.sh (start|stop|restart) <command> "
 
 # if no args specified, show usage
@@ -49,7 +32,7 @@ start(){
     fi
   fi
   echo starting $command, logging to $log
-  exec_command="$SH_DIR/prometheus --config.file=prometheus.yml --web.listen-address=:9090 --web.enable-lifecycle --storage.tsdb.path=/data/prometheus/data/"
+  exec_command="$SH_DIR/prometheus --config.file=prometheus.yml --web.listen-address=:9090 --web.enable-lifecycle --storage.tsdb.retention.time=30d --storage.tsdb.path=/data/prometheus/data/"
   echo "nohup $exec_command > $log 2>&1 &"
   nohup $exec_command > $log 2>&1 &
   echo $! > $pid
