8865108b75047e79d108b3b979673f69f0c2db27
zhanghuangbin <1173850085@qq.com>
2025-12-24 18:48:02 +0800

    fix: 修复优化全局变量引起的bug(漏改代码)
    bug来源：a9f83fe75602a8aba5a09e845c7b2482f7c56221

--- datasophon-service/src/main/java/com/datasophon/api/service/impl/ClusterServiceDashboardServiceImpl.java
+++ datasophon-service/src/main/java/com/datasophon/api/service/impl/ClusterServiceDashboardServiceImpl.java
@@ -61,7 +61,7 @@ public class ClusterServiceDashboardServiceImpl
     @Override
     public String getGrafanaHost(Integer clusterId) {
         Map<String, String> globalVariables = GlobalVariables.getVariables(clusterId);
-        return globalVariables.get("${GRAFANA.${grafanaHost}}") + ":3000";
+        return globalVariables.get("${Grafana.__hostIp__}") + ":3000";
     }
     
     @Override
