fcf99c279499603d91b3139c55e1ca062ee9901d
zhanghuangbin <1173850085@qq.com>
2025-12-24 16:10:30 +0800

    fix: 修复增加api前缀后接口地址冲突

--- datasophon-api/src/main/java/com/datasophon/api/controller/ClusterRoleUserController.java
+++ datasophon-api/src/main/java/com/datasophon/api/controller/ClusterRoleUserController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("cluster/user")
+@RequestMapping("cluster/roleUser")
 public class ClusterRoleUserController extends ApiController {
     
     @Autowired
