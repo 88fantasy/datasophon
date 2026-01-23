8456d3be8a8c4ec7e13d0c5fc277f078aa1b3969
zhanghuangbin <1173850085@qq.com>
2026-01-23 16:23:28 +0800

    fix: 增加重试次数

--- datasophon-worker/src/main/java/com/datasophon/worker/actor/ServiceStatusActor.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/actor/ServiceStatusActor.java
@@ -2,6 +2,7 @@ package com.datasophon.worker.actor;
 
 import com.datasophon.common.command.ServiceRoleOperateCommand;
 import com.datasophon.common.utils.ExecResult;
+import com.datasophon.common.utils.PropertyUtils;
 import com.datasophon.worker.handler.ServiceHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +20,21 @@ public class ServiceStatusActor extends HookTypedActor<ServiceRoleOperateCommand
         ExecResult result = new ExecResult();
         try {
             ServiceHandler serviceHandler = new ServiceHandler(command.getServiceName(), command.getServiceRoleName());
-            result = serviceHandler.status(command.getStatusRunner(), command.getDecompressPackageName());
+            int times = PropertyUtils.getInt("times");
+            int count = 0;
+            while (count < times) {
+                count++;
+                result = serviceHandler.status(command.getStatusRunner(), command.getDecompressPackageName());
+                if (result.getExecResult()) {
+                    break;
+                } else {
+                    try {Thread.sleep(5 * 1000);} catch (InterruptedException ignored) {}
+                }
+            }
+            if (count == times) {
+                result.setExecResult(false);
+                result.setExecOut(String.format("检查%s状态失败，已经达到重试次数%s", command.getServiceRoleName(), times));
+            }
         } catch (Throwable throwable) {
             result = ExecResult.error(String.format("检查%s状态失败，%s", command.getServiceRoleName(), throwable.getMessage()));
             log.error("检查{}状态失败", command.getServiceRoleName(), throwable);
