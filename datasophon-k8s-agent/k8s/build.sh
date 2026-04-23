9977ebb05916329e9e78bfdd2bafa211b7c8c444
zhanghuangbin <1173850085@qq.com>
2026-04-23 14:22:52 +0800

    feat: 增加多架构构建脚本

---
+++ datasophon-k8s-agent/k8s/build.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+tag="${1:-latest}"
+shift 2>/dev/null || true
+
+proxy=""
+arch=""
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --proxy)
+      proxy="$2"
+      shift 2
+      ;;
+    --arch)
+      arch="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -n "$proxy" ]; then
+  export https_proxy="$proxy"
+  export http_proxy="$proxy"
+  echo "use proxy: $proxy."
+fi
+
+image_name="datasophon-k8s-agent"
+
+if [ "$arch" = "all" ]; then
+  platform="linux/amd64,linux/arm64"
+elif [ -n "$arch" ]; then
+  platform="linux/${arch}"
+fi
+
+if [ "$arch" = "all" ]; then
+  BUILDER_NAME="datasophon-build"
+
+  builders=$(docker buildx ls)
+  if echo "${builders}" | grep -q "${BUILDER_NAME}"; then
+    echo "BuildKit builder '${BUILDER_NAME}' already exists."
+  else
+    echo "BuildKit builder '${BUILDER_NAME}' does not exist."
+    docker buildx create --name ${BUILDER_NAME} --platform linux/amd64,linux/arm64 --use --bootstrap
+  fi
+
+  docker buildx build \
+   --builder ${BUILDER_NAME} \
+   --platform=${platform} \
+   --progress plain \
+   --output type=oci,dest=./${image_name}-${tag}-image-all.tar \
+   -t ${image_name}:${tag} .
+elif [ -n "$arch" ]; then
+  docker build \
+   --platform=${platform} \
+   --progress plain \
+   --output type=docker,dest=./${image_name}-${tag}-image-$arch.tar \
+   -t ${image_name}:${tag} .
+else
+  docker build \
+   --progress plain \
+   --output type=docker,dest=./${image_name}-${tag}-image-default.tar \
+   -t ${image_name}:${tag} .
+fi
